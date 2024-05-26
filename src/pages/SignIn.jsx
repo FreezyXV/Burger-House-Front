0adcbe88774f0da768773643ffa472db6cb9a6d5
FreezyXV <i_841@mail.ru>
2024-05-26 17:17:20 +0200

    Admin resolve

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -34,11 +34,14 @@ function SignIn() {
     }
 
     try {
-      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/users/login`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, password }),
-      });
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/users/login`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ username, password }),
+        }
+      );
       if (!response.ok) {
         const errorData = await response.json();
         throw new Error(errorData.message || "Invalid username or password.");
